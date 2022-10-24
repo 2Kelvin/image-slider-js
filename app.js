5327e00d816f4f4b94de74debcd4c9e5ca2f9b3c
2Kelvin <kvin.mwangi254@gmail.com>
2022-10-24 15:18:33 +0300

    slideshow done

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const index = 0;
-const image_array = [];
-const slide_time = 3000;
+let i = 0;
+const images_array = [];
+
+// adding images to the array
+images_array[0] = "images/animal_1.jpg";
+images_array[1] = "images/animal_2.jpg";
+images_array[2] = "images/animal_3.jpg";
+images_array[3] = "images/animal_4.jpg";
+images_array[4] = "images/animal_5.jpg";
+images_array[5] = "images/animal_6.jpg";
+
+function slideImage() {
+    document.getElementById('slide').src = images_array[i];
+
+    if (i < images_array.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("slideImage()", 2500);
+}
+
+slideImage();
